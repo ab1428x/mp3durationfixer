661fd1f3961b351b5ab2523d7ee1769b25f00940
Jay <arisnr1client@gmail.com>
2019-04-16 13:59:48 +0200

    init

---
+++ MP3DurationFixer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MP3DurationFixer
+//
+//  Created by Jay on 4/16/19.
+//  Copyright © 2019 Andrzej Baruk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
